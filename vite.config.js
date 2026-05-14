ab63f88277f1e9a148ae71b5743d2cf46fe3a5df
jackyw05 <jackyw2500@gmail.com>
2026-05-13 23:13:37 -0400

    fixed issues with hosting on vercel

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/restaurant-website/',
+  base: '/',
   server: {
     proxy: {
       '/api': 'http://localhost:4000'
